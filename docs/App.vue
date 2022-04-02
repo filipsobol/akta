38ea263ca1c6f210c24c1eae1e28098a9520c143
Filip Sobol <sobol.filip@gmail.com>
2022-04-02 17:49:16 +0200

    docs: minor styling update

--- docs/App.vue
+++ docs/App.vue
@@ -6,7 +6,7 @@
       <router-link to="/routing">Routing</router-link>
     </nav>
 
-    <div class="container mx-auto prose prose-a:text-red-600 prose-h2:border-t prose-h2:border-gray-200 prose-h2:pt-8 p-2 md:max-w-[38rem] lg:max-w-[48rem]">
+    <div class="container mx-auto prose prose-a:decoration-red-200 prose-a:decoration-2 prose-a:underline-offset-[5px] prose-h2:border-t prose-h2:border-gray-200 prose-h2:pt-8 p-2 pb-20 md:max-w-[38rem] lg:max-w-[48rem]">
       <router-view />
     </div>
   </div>
